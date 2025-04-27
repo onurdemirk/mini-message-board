fcd92730080d7c3aa98c530c95a33159178332f3
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-04-27 20:07:46 +0200

    Add app level Express middleware

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
 
 const mainRouter = require('./routes/index')
