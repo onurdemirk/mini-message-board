74fd3f64219958ce475b2acd8052cf8910ed0b31
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-04-27 20:43:40 +0200

    Add favicon

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const path = require("node:path");
 const app = express();
 
+const favicon = require('serve-favicon');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -17,7 +21,7 @@ app.use("/new-message", mainRouter)
 app.use((req, res) => {
     res.status(404).render('404');
   });
-  
+
 app.listen(PORT, () => {
     console.log(`Server is working: http://localhost:${PORT}`)
 })
