da484042b6b1e64caa68a65c00b6fd63c557bee1
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-04-27 19:36:22 +0200

    Set up Express app in app.js: configure EJS view engine and mount routers

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const path = require("node:path");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const PORT = 3000;
+
+const mainRouter = require('./routes/index')
+
+app.use("/", mainRouter)
+app.use("/new-message", mainRouter)
+
+
+app.listen(PORT, () => {
+    console.log(`Server is working: http://localhost:${PORT}`)
+})
